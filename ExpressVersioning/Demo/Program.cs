e0edf4a27ba2c4bdc3ece9f9d0f69a366e62a105
Sangeeth Nandakumar <sangeethnandakumarwork@gmail.com>
2020-08-01 12:32:32 +0530

    Initial Commit

---
+++ ExpressVersioning/Demo/Program.cs
@@ -0,0 +1,18 @@
+﻿using ExpressVersioning;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Demo
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            var app = new AppVersioning(VersioningEntity.APPLICATION, VersioningSchema.DEFAULT);
+            var version = app.VersionRunningAssembly();
+            Console.WriteLine($"Application version is - " + version);
+            Console.Read();
+        }
+    }
+}
---
+++ ExpressVersioning/ExpressVersioning/AppVersioning.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExpressVersioning
+{
+    public class AppVersioning
+    {
+        private int major = 0;
+        private int minor = 0;
+        private int build = 0;
+        private int revision = 0;
+
+        private readonly VersioningSchema _schema;
+        private readonly VersioningEntity _entity;
+        
+        public AppVersioning(VersioningEntity entity, VersioningSchema schema)
+        {
+            _schema = schema;
+            _entity = entity;
+        }
+
+        public string VersionRunningAssembly()
+        {
+            var verion = $"{major}.{minor}.{build}.{revision}";
+            return verion;
+        }
+    }
+}
